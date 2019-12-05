b1b08ede38c2db5850060f29d4955bb06d479a1a
Vadnir Vieira <vadyy2014@gmail.com>
2019-12-05 17:51:41 -0100

    Finalizado parte notificacao

--- src/components/Notification/index.js
+++ src/components/Notification/index.js
@@ -1,6 +1,10 @@
-import React from 'react';
-
+import React, { useState, useEffect, useMemo } from 'react';
 import { MdNotifications } from 'react-icons/md';
+import { parseISO, formatDistance } from 'date-fns';
+import pt from 'date-fns/locale/pt';
+
+import api from '~/services/api';
+
 import {
     Container,
     Badge,
@@ -10,29 +14,79 @@ import {
 } from './styles';
 
 export default function Notification() {
+    const [visible, setVisible] = useState(false);
+    const [notifications, setNotifications] = useState([]);
+
+    const hasUnread = useMemo(
+        () =>
+            !!notifications.find(notification => notification.read === false)[
+                notifications
+            ]
+    );
+
+    useEffect(() => {
+        async function loadNotifications() {
+            const response = await api.get('notifications');
+
+            const data = response.data.map(notification => ({
+                ...notification,
+                timeDistance: formatDistance(
+                    parseISO(notification.createdAt),
+                    new Date(),
+                    { addSuffix: true, locale: pt }
+                ),
+            }));
+
+            setNotifications(data);
+        }
+
+        loadNotifications();
+    }, []);
+
+    function handleToggleVisible() {
+        setVisible(!visible);
+    }
+
+    async function handleMarkHasRead(id) {
+        await api.put(`notifications/${id}`);
+
+        setNotifications(
+            notifications.map(notification =>
+                notification._id === id
+                    ? { ...notification, read: true }
+                    : notification
+            )
+        );
+    }
+
     return (
         <Container>
-            <Badge hasUnreade>
+            <Badge onClick={handleToggleVisible} hasUnreade={hasUnread}>
                 <MdNotifications color="#7159c1" size={20} />
             </Badge>
 
-            <NotificationList>
+            <NotificationList visible={visible}>
                 <Scroll>
-                    <Notifications unread>
-                        <p> Voce possui um novo agendamento para amanha </p>
-                        <tile> Ha 2 dias </tile>
-                        <button type="button"> Marcar como lida</button>
-                    </Notifications>
-                    <Notifications>
-                        <p> Voce possui um novo agendamento para amanha </p>
-                        <tile> Ha 2 dias </tile>
-                        <button type="button"> Marcar como lida</button>
-                    </Notifications>
-                    <Notifications>
-                        <p> Voce possui um novo agendamento para amanha </p>
-                        <tile> Ha 2 dias </tile>
-                        <button type="button"> Marcar como lida</button>
-                    </Notifications>
+                    {notifications.map(notification => (
+                        <Notifications
+                            key={notification._id}
+                            unread={!notification.read}
+                        >
+                            <p> {notification.content} </p>
+                            <tile> {notification.timeDistance} </tile>
+                            {!notification.read && (
+                                <button
+                                    type="button"
+                                    onClick={() =>
+                                        handleMarkHasRead(notification._id)
+                                    }
+                                >
+                                    {' '}
+                                    Marcar como lida
+                                </button>
+                            )}
+                        </Notifications>
+                    ))}
                 </Scroll>
             </NotificationList>
         </Container>
